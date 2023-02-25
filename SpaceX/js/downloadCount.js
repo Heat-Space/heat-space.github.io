668f2a18f5c0d129329e912089928aa2ba977fd1
Heat-Space <109669255+Heat-Space@users.noreply.github.com>
2023-02-25 20:08:16 +0300

    Update

---
+++ SpaceX/js/downloadCount.js
@@ -0,0 +1,12 @@
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      document.getElementById("message").innerHTML = " " + localStorage.clickcount + " Downloads";
+    } else {
+      document.getElementById("message").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
